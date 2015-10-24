349d7dc32793f93d94e0d437dfedda1d1828aa2a
jonte <u@h>
2015-10-24 23:24:55 +0200

    clean

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
-var fs = require('fs'),
-    path = require('path')
-    db = require('./lib/db'),
-    previewfile = require('./lib/previewfile'),
-    glob = require('glob'),
-    wallpaper = require('wallpaper');
+var db = require('./lib/db'),
+	previewfile = require('./lib/previewfile'),
+	glob = require('glob');
 
 var lightRoomPath = process.env.HOME + "/Pictures/Lightroom/Lightroom 5 Catalog Previews.lrdata/"
 
 module.exports.getRandomFile = function(cb) {
-    db.getIds(function (ids) {
-        var id = ids[randomInt(0, ids.length-1)];
-        console.log(id);
+	db.getIds(function (ids) {
+			var id = ids[randomInt(0, ids.length-1)];
+			console.log(id);
 
-        glob(lightRoomPath + id.slice(0, 1) + "/" + id.slice(0, 4) + "/**/" + id + "*.lrprev", function (er, files) {
-            console.log(files);
+			glob(lightRoomPath + id.slice(0, 1) + "/" + id.slice(0, 4) + "/**/" + id + "*.lrprev", function (er, files) {
+					console.log(files);
 
-            var jpegBuf = previewfile.getJpeg(files[0]);
-            var filename = "wallpaper-" + randomInt(0, 1000) + ".jpg";
-            fs.writeFileSync(filename, jpegBuf);
-            console.log("Wrote wallpaper: " + jpegBuf.length + "b");
+					var jpegBuf = previewfile.getJpeg(files[0]);
+					console.log("Extracted jpeg: " + jpegBuf.length + "b");
 
-            cb(path.resolve(filename));
-        });
-    });
+					cb(jpegBuf);
+			});
+	});
 }
-
 function randomInt (low, high) {
     return Math.floor(Math.random() * (high - low) + low);
 }
--- test.js
+++ test.js
@@ -1,8 +1,18 @@
-var lightwall = require('./index.js');
+var fs = require('fs'),
+    path = require('path'),
+    wallpaper = require('wallpaper'),
+    lightwall = require('./index.js');
 
-lightwall.getRandomFile(function(path) {
-  wallpaper.set(path, function (err) {
-     console.log('done');
- });
+
+lightwall.getRandomFile(function(buf) {
+    var filename = "wallpaper-" + randomInt(0, 1000) + ".jpg";
+    fs.writeFileSync(filename, buf);
+ 
+    wallpaper.set(path.resolve(filename), function (err) {
+        console.log('done');
+    });
 })
 
+function randomInt (low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+}
